76ed3f67a3dc86d2ee4d4c4d864fe3367a8a00fc
ZedFixed <ronbernarte0@gmail.com>
2025-09-19 14:34:56 +0800

    Update 9182727371_new.lua

--- 9182727371_new.lua
+++ 9182727371_new.lua
@@ -79,9 +79,9 @@ do
         task.spawn(function()
             while FPSDevourer.running and not FPSDevourer._stop do
                 equipTools()
-                task.wait(0.005)
+                task.wait(0.015)
                 unequipTools()
-                task.wait(0.005)
+                task.wait(0.015)
             end
         end)
     end
